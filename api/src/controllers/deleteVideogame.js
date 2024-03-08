58888dbc5a1e20269b40c9c2dc3753017f53d202
BlackShadowBug <ale_95_marce@hotmail.com>
2024-03-08 18:59:02 -0300

    PI

---
+++ api/src/controllers/deleteVideogame.js
@@ -0,0 +1,22 @@
+const { Videogame } = require("../db_conection");
+
+const deleteVideogame = async (req, res) => {
+    const { idVideogame } = req.params;
+
+    try {
+        const deletedVideogame = await Videogame.destroy({
+            where: { id: idVideogame }
+        });
+
+        if (!deletedVideogame) {
+            return res.status(404).json({ message: "No se encontró el videojuego con el ID proporcionado" });
+        }
+
+        res.json({ message: "Videojuego eliminado exitosamente" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Hubo un error al intentar eliminar el videojuego" });
+    }
+};
+
+module.exports = { deleteVideogame };
--- api/src/routes/videogamesRouter.js
+++ api/src/routes/videogamesRouter.js
@@ -5,10 +5,14 @@ const { getVideogameById}=require('../controllers/getById')
 const { searchVideogameByName }=require('../controllers/searchByName')
 const { createVideogame}=require('../controllers/createGame')
 
+const { deleteVideogame } = require('../controllers/deleteVideogame');
+
 videogamesRouter.get('/',getVideogames)
 videogamesRouter.get("/id/:idVideogame", getVideogameById);
 videogamesRouter.get("/name/", searchVideogameByName);
 videogamesRouter.post("/create", createVideogame);
+
+videogamesRouter.delete("/delete/:idVideogame", deleteVideogame);
 
 module.exports={
     videogamesRouter,
